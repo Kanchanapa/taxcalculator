1132365d9e975b7459ed15634ba2d41e517e24d1
Kanchanapa <65842176+Kanchanapa@users.noreply.github.com>
2020-05-24 11:47:06 +0700

    Add files via upload

---
+++ main1.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace _2.Pre_ProjectFinal
+{
+    partial class main1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main1));
+            this.Head = new System.Windows.Forms.Label();
+            this.NEXT = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.name = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.othermney = new System.Windows.Forms.NumericUpDown();
+            this.bonus = new System.Windows.Forms.NumericUpDown();
+            this.label12 = new System.Windows.Forms.Label();
+            this.moneyyear = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.calculate = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.month = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.othermney)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bonus)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Head
+            // 
+            this.Head.AutoSize = true;
+            this.Head.Font = new System.Drawing.Font("Britannic Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Head.ForeColor = System.Drawing.SystemColors.Window;
+            this.Head.Location = new System.Drawing.Point(19, 22);
+            this.Head.Name = "Head";
+            this.Head.Size = new System.Drawing.Size(228, 82);
+            this.Head.TabIndex = 3;
+            this.Head.Text = "       TAX\r\nCALCULATOR";
+            // 
+            // NEXT
+            // 
+            this.NEXT.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.NEXT.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.NEXT.Location = new System.Drawing.Point(354, 260);
+            this.NEXT.Name = "NEXT";
+            this.NEXT.Size = new System.Drawing.Size(75, 23);
+            this.NEXT.TabIndex = 4;
+            this.NEXT.Text = "NEXT >";
+            this.NEXT.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Wheat;
+            this.label1.Location = new System.Drawing.Point(101, 110);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Welcome ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cloud", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(19, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "คุณ";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Location = new System.Drawing.Point(53, 161);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(198, 1);
+            this.panel1.TabIndex = 8;
+            // 
+            // name
+            // 
+            this.name.BackColor = System.Drawing.Color.MidnightBlue;
+            this.name.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.name.Font = new System.Drawing.Font("Quark", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.ForeColor = System.Drawing.SystemColors.Window;
+            this.name.Location = new System.Drawing.Point(53, 138);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(196, 22);
+            this.name.TabIndex = 9;
+            this.name.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            this.errorProvider1.Tag = "Caution";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-5, 156);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(166, 173);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // othermney
+            // 
+            this.othermney.Location = new System.Drawing.Point(341, 131);
+            this.othermney.Margin = new System.Windows.Forms.Padding(2);
+            this.othermney.Name = "othermney";
+            this.othermney.Size = new System.Drawing.Size(99, 20);
+            this.othermney.TabIndex = 65;
+            this.othermney.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // bonus
+            // 
+            this.bonus.Location = new System.Drawing.Point(341, 96);
+            this.bonus.Margin = new System.Windows.Forms.Padding(2);
+            this.bonus.Name = "bonus";
+            this.bonus.Size = new System.Drawing.Size(99, 20);
+            this.bonus.TabIndex = 64;
+            this.bonus.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label12.Location = new System.Drawing.Point(280, 156);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(61, 26);
+            this.label12.TabIndex = 63;
+            this.label12.Text = "**หากไม่มี\r\n  ให้กรอก 0";
+            // 
+            // moneyyear
+            // 
+            this.moneyyear.BackColor = System.Drawing.Color.MidnightBlue;
+            this.moneyyear.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.moneyyear.Font = new System.Drawing.Font("Angsana New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.moneyyear.ForeColor = System.Drawing.Color.Pink;
+            this.moneyyear.Location = new System.Drawing.Point(330, 216);
+            this.moneyyear.Name = "moneyyear";
+            this.moneyyear.Size = new System.Drawing.Size(105, 38);
+            this.moneyyear.TabIndex = 62;
+            this.moneyyear.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("WR Tish Kid", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.SystemColors.Window;
+            this.label11.Location = new System.Drawing.Point(442, 221);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(39, 25);
+            this.label11.TabIndex = 61;
+            this.label11.Text = "บาท";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("WR Tish Kid", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label10.Location = new System.Drawing.Point(247, 220);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(77, 25);
+            this.label10.TabIndex = 60;
+            this.label10.Text = "รายได้ต่อปี";
+            // 
+            // calculate
+            // 
+            this.calculate.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.calculate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.calculate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.calculate.Font = new System.Drawing.Font("WR Tish Kid", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculate.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.calculate.Location = new System.Drawing.Point(347, 164);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(87, 22);
+            this.calculate.TabIndex = 59;
+            this.calculate.Text = "คำนวณรายได้";
+            this.calculate.UseVisualStyleBackColor = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("WR Tish Kid", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label8.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label8.Location = new System.Drawing.Point(274, 33);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(188, 25);
+            this.label8.TabIndex = 56;
+            this.label8.Text = "1. กรุณากรอกรายได้ของคุณ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("WR Tish Kid", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.Window;
+            this.label7.Location = new System.Drawing.Point(448, 134);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 20);
+            this.label7.TabIndex = 55;
+            this.label7.Text = "บาท";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("WR Tish Kid", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Window;
+            this.label6.Location = new System.Drawing.Point(448, 99);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 20);
+            this.label6.TabIndex = 54;
+            this.label6.Text = "บาท";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("WR Tish Kid", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Window;
+            this.label5.Location = new System.Drawing.Point(275, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 20);
+            this.label5.TabIndex = 53;
+            this.label5.Text = "เงินอื่น ๆ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("WR Tish Kid", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Window;
+            this.label4.Location = new System.Drawing.Point(290, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 20);
+            this.label4.TabIndex = 52;
+            this.label4.Text = "โบนัส";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("WR Tish Kid", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(448, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 20);
+            this.label3.TabIndex = 51;
+            this.label3.Text = "บาท";
+            // 
+            // month
+            // 
+            this.month.Font = new System.Drawing.Font("Microsoft PhagsPa", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.month.Location = new System.Drawing.Point(341, 61);
+            this.month.Name = "month";
+            this.month.Size = new System.Drawing.Size(100, 22);
+            this.month.TabIndex = 50;
+            this.month.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("WR Tish Kid", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.SystemColors.Window;
+            this.label13.Location = new System.Drawing.Point(275, 61);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(51, 20);
+            this.label13.TabIndex = 49;
+            this.label13.Text = "เงินเดือน";
+            // 
+            // main1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(508, 318);
+            this.Controls.Add(this.othermney);
+            this.Controls.Add(this.bonus);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.moneyyear);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.month);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NEXT);
+            this.Controls.Add(this.Head);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "main1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tax Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.othermney)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bonus)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label Head;
+        private System.Windows.Forms.Button NEXT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        public System.Windows.Forms.NumericUpDown othermney;
+        public System.Windows.Forms.NumericUpDown bonus;
+        private System.Windows.Forms.Label label12;
+        public System.Windows.Forms.TextBox moneyyear;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.TextBox month;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
